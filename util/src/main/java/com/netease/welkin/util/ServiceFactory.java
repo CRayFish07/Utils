d2944f683bd7a375afae40859a389d6aaeda82b6
cqbc <457410043@qq.com>
2016-09-26 13:14:31 +0800

    提交sessionUtil

---
+++ util/src/main/java/com/netease/welkin/util/ServiceFactory.java
@@ -0,0 +1,51 @@
+package com.netease.welkin.util;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zxwu
+ */
+@Component
+public final class ServiceFactory {
+
+    private ServiceFactory() {
+    }
+
+    private static ServiceFactory serviceFactory;
+    private static ApplicationContext context;
+
+    /**
+     * @return ServiceFactory
+     */
+    public static ServiceFactory getInstance() {
+        if (serviceFactory == null) {
+            context = new ClassPathXmlApplicationContext("conf/applicationContext.xml");
+            serviceFactory = (ServiceFactory) context.getBean("serviceFactory");
+        }
+        return serviceFactory;
+    }
+
+    /**
+     * 根据提供的bean名称得到相应的服务类
+     * 
+     * @param name bean名称
+     * @return Object
+     */
+    public Object getBean(String name) {
+        return context.getBean(name);
+    }
+
+    /**
+     * 根据提供的bean class得到相应的服务类
+     * 
+     * @param <T> <T>
+     * @param clazz clazz
+     * @return <T>
+     */
+    public <T> T getBean(Class<T> clazz) {
+        return context.getBean(clazz);
+    }
+
+}
---
+++ util/src/main/java/com/netease/welkin/util/SessionUtil.java
@@ -0,0 +1,70 @@
+package com.netease.welkin.util;
+
+import java.util.UUID;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Session工具类
+ * 
+ * @author chen liu
+ */
+public abstract class SessionUtil {
+    private SessionUtil() {
+    }
+
+    /**
+     * Cookie名称
+     */
+    private static final String SESSION_ID = "NETEASE_LIVE_SESSION_ID";
+
+    /**
+     * 获取Session(从Cookie中获取session id)
+     * 
+     * @param req HttpServletRequest
+     * @return BbsSession
+     */
+    public static BbsSession getSession(HttpServletRequest req) {
+        return getSession(req, null);
+    }
+
+    /**
+     * 获取Session(从Cookie中获取session id)
+     * 
+     * @param req HttpServletRequest
+     * @param res HttpServletResponse 当res不为空时，如果session不存在，会自动创建，并生成cookie
+     * @return BbsSession
+     */
+    public static BbsSession getSession(HttpServletRequest req, HttpServletResponse res) {
+        String sid = null;
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equalsIgnoreCase(SESSION_ID)) {
+                    sid = c.getValue();
+                }
+            }
+        }
+        if (sid == null) {
+            // 找不到session
+            if (res != null) {
+                sid = UUID.randomUUID().toString();
+
+                // 创建BBS_SESSION_ID的Cookie
+                Cookie cookie = new Cookie(SESSION_ID, sid);
+//                Cookie cookie = new Cookie("", sid);
+                cookie.setMaxAge(-1);
+                cookie.setDomain(req.getServerName());
+                System.err.println("domain="+cookie.getDomain());
+                cookie.setPath("/");
+                res.addCookie(cookie);
+            } else {
+                return null;
+            }
+        }
+        return new BbsSessionImpl(sid);
+    }
+}
